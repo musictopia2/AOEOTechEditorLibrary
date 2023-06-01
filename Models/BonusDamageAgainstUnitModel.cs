bdf09fd5ba6612b47bc5acea08a26abbeceea7b3
Andy Walter <musictopia2@hotmail.com>
2023-06-01 12:51:30 -0700

    Added new bonus damage against unit model. Also, the extra parameter for unit type is put in there as well.

---
+++ Models/BonusDamageAgainstUnitModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AOEOTechEditorLibrary.Models;
+public class BonusDamageAgainstUnitModel : UnitModel
+{
+    private readonly string _bonusDamageAgainst;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="bonusDamageAgainst">This is the unit that it does bonus damage against.  for example, if you choose infantry, then the unit does bonus damage against infantry</param>
+    public BonusDamageAgainstUnitModel(string bonusDamageAgainst)
+    {
+        _bonusDamageAgainst = bonusDamageAgainst;
+    }
+    protected override string Action => "";
+    protected override string SubType => "DamageBonus";
+    protected override string Relativity => "Percent";
+    protected override string UnitType => _bonusDamageAgainst;
+    protected override string Resource => "";
+    protected override string DamageType => "";
+}
--- Models/BonusDamageProtectionModel.cs
+++ Models/BonusDamageProtectionModel.cs
@@ -1,20 +1,10 @@
 ﻿namespace AOEOTechEditorLibrary.Models;
 public class BonusDamageProtectionModel : UnitModel
 {
-    private readonly string _bonusDamageAgainst;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="bonusDamageAgainst">This is the unit that it does bonus damage against.  for example, if you choose infantry, then the unit does bonus damage against infantry</param>
-    public BonusDamageProtectionModel(string bonusDamageAgainst)
-    {
-        _bonusDamageAgainst = bonusDamageAgainst;
-    }
     protected override string Action => "";
     protected override string SubType => "DamageBonusReduction";
     protected override string Relativity => "Percent";
-    protected override string UnitType => _bonusDamageAgainst;
+    protected override string UnitType => "";
     protected override string Resource => "";
     protected override string DamageType => "";
 }
