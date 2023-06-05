3c099b75f374ed80bf7d7cbf50b45db1aec45845
Andy Walter <musictopia2@hotmail.com>
2023-06-05 10:55:35 -0700

    Added tech that is active for some techs (especially for wonder).

---
+++ Models/TectActiveEffectModel.cs
@@ -0,0 +1,25 @@
+﻿namespace AOEOTechEditorLibrary.Models;
+public class TectActiveEffectModel : BasicEffectModel
+{
+    protected override string Action => "";
+    protected override string SubType => "";
+    protected override string Relativity => "";
+    protected override string UnitType => "";
+    protected override string Resource => "";
+    protected override string DamageType => "";
+    protected override string EffectType => "TechStatus";
+    protected override string TargetType => "";
+    protected override string Status => "active";
+    protected override void Start()
+    {
+        if (ProtoUnit != "")
+        {
+            ProtoUnit = "";
+        }
+        if (Content == "")
+        {
+            throw new CustomBasicException("Must have the tech that is enabled.  Goes under Content");
+        }
+        base.Start();
+    }
+}
