ed8a71d70c07b6d7dbeb307c606285150d467b8d
Andy Walter <musictopia2@hotmail.com>
2023-06-06 10:07:29 -0700

    Added new model for conversion resistance.

---
+++ Models/ConvertResistModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AOEOTechEditorLibrary.Models;
+public class ConvertResistModel : UnitModel
+{
+    protected override string Action => "";
+    protected override string SubType => "ConvertResist";
+    protected override string UnitType => "";
+    protected override string Resource => "";
+    protected override string DamageType => "";
+    protected override string Relativity => "Percent";
+}
