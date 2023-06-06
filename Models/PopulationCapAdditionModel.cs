044e777c35411b8f88004ed4664b3577d698ac1a
Andy Walter <musictopia2@hotmail.com>
2023-06-06 13:42:26 -0700

    New model for population cap addition. Useful so the wonder itself can provide additional population.

---
+++ Models/PopulationCapAdditionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AOEOTechEditorLibrary.Models;
+public class PopulationCapAdditionModel : UnitModel
+{
+    protected override string Action => "";
+    protected override string SubType => "PopulationCapAddition";
+    protected override string Relativity => "Assign";
+    protected override string UnitType => "";
+    protected override string Resource => "";
+    protected override string DamageType => "";
+}
