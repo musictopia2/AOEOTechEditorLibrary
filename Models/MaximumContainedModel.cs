014570e6867c22472779a3f98b0362d04502dc65
Andy Walter <musictopia2@hotmail.com>
2023-10-04 20:16:10 -0700

    Added maximum contained so the babylon siege towers can have less units inside each one but do more damage to make up for it.

---
+++ Models/MaximumContainedModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AOEOTechEditorLibrary.Models;
+public class MaximumContainedModel : UnitModel
+{
+    protected override string Action => "";
+    protected override string SubType => "MaximumContained";
+    protected override string Relativity => "Absolute";
+    protected override string UnitType => "";
+    protected override string Resource => "";
+    protected override string DamageType => "";
+    protected override void Start()
+    {
+        if (ProtoUnit != BabylonUnits.SiegeTower)
+        {
+            throw new CustomBasicException("Only the babylon siege towers are allowed to change the maximum contained");
+        }
+        base.Start();
+    }
+}
