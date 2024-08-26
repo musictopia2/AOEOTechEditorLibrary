0e3634620bae713c07703bff0e1941806a8987ae
Andy Walter <musictopia2@hotmail.com>
2024-08-26 10:25:40 -0700

    Added a new effect that allows custom traits.  A person using must specify the action. Allows to train units automatically.  This is for advanced custom scenarios.

---
+++ Models/CustomTacticEffect.cs
@@ -0,0 +1,19 @@
+﻿namespace AOEOTechEditorLibrary.Models;
+public class CustomTacticEffect() : UnitModel
+{
+    public string CustomTacticName { get; set; } = ""; //was going to require to put into constructors.  the problem is for now, i can't serialize/deserialize.
+    protected override string Action => CustomTacticName;
+    protected override string SubType => "ActionEnable";
+    protected override string UnitType => "";
+    protected override string Resource => "";
+    protected override string DamageType => "";
+    protected override string Relativity => "Absolute";
+    protected override void Start()
+    {
+        if (CustomTacticName == "")
+        {
+            throw new CustomBasicException("Must specify a tactics name to be used since it may not be known at design time");
+        }
+        base.Start();
+    }
+}
