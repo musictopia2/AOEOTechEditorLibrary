d155cb673ec4c6c6dc744102b6a48b7b2023cfcb
Andy Walter <musictopia2@hotmail.com>
2025-05-01 15:51:52 -0700

    Add PrereqsServices class with GetPrereqs method
    Introduces a new static class `PrereqsServices` in the
    `AOEOTechEditorLibrary.Helpers` namespace. This class
    features the `GetPrereqs` method, which processes a list
    of `BasicPrereqModel` objects and returns a list of
    corresponding `XElement` objects.

---
+++ Helpers/PreReqsServices.cs
@@ -0,0 +1,14 @@
+﻿namespace AOEOTechEditorLibrary.Helpers;
+public static class PrereqsServices
+{
+    public static BasicList<XElement> GetPrereqs(BasicList<BasicPrereqModel> prereqs)
+    {
+        BasicList<XElement> output = [];
+        foreach (BasicPrereqModel prereq in prereqs)
+        {
+            XElement xml = prereq.GetElement();
+            output.Add(xml);
+        }
+        return output;
+    }
+}
