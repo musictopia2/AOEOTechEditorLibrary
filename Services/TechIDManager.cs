8339dec4db73c7b91f8898b815dfb9e03a070f37
Andy Walter <musictopia2@hotmail.com>
2025-05-09 18:19:27 -0700

    Add TechIDManager and TechNameService classes
    Introduce `TechIDManager` for managing unique tech IDs and
    `TechNameService` for generating unique names with prefixes.
    Both classes enhance code readability and maintainability
    using modern C# features.

---
+++ Services/TechIDManager.cs
@@ -0,0 +1,18 @@
+﻿namespace AOEOTechEditorLibrary.Services;
+public class TechIDManager
+{
+    private int _currentID;
+    public int GetNextID => _currentID;
+    public void RegisterTech()
+    {
+        _currentID++; //this means will increment the id.
+    }
+    public void Clear()
+    {
+        //this needs to clear the new id manager.
+        string path = dd1.RawTechLocation;
+        XElement source = XElement.Load(path);
+        var list = source.Elements().ToBasicList();
+        _currentID = list.Count;
+    }
+}
---
+++ Services/TechNameService.cs
@@ -0,0 +1,11 @@
+﻿namespace AOEOTechEditorLibrary.Services;
+public class TechNameService
+{
+    private readonly Dictionary<string, int> _counters = new();
+    public string GetNext(string prefix)
+    {
+        int count = _counters.TryGetValue(prefix, out var current) ? current + 1 : 1;
+        _counters[prefix] = count;
+        return $"{prefix}{count}";
+    }
+}
