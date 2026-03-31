6bba40f4e06167984a762faf97768aced0781dd3
Andy Walter <musictopia2@hotmail.com>
2026-03-31 07:34:39 -0700

    Added strongly typed list for relative class.  i know of 3 options currently.   if another one becomes known, will add.

---
+++ Helpers/RelativeClass.cs
@@ -0,0 +1,7 @@
+﻿namespace AOEOTechEditorLibrary.Helpers;
+public static class RelativeClass
+{
+    public const string Absolute = "Absolute";
+    public const string Percent = "Percent";
+    public const string Assign = "Assign";
+}
